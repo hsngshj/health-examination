5a70aae38aecb7b49beb138fa8b80ccca61aa263
Ubuntu <ubuntu@ip-172-31-12-101.ap-northeast-1.compute.internal>
2023-02-04 09:28:04 +0000

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-    root to: 'toppages#index'
+  root to: 'toppages#index'
     
-    resources :users
-    resources :health_examination_results
+  get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :users, except: [:new]
+  resources :health_examination_results
 end
