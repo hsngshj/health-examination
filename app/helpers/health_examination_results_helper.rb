143916e172308cb28d86343b3783fae6f1929ba7
Ubuntu <ubuntu@ip-172-31-12-101.ap-northeast-1.compute.internal>
2023-02-14 12:08:39 +0000

    HealthExaminationResultsHelper changed by higuchi

--- app/helpers/health_examination_results_helper.rb
+++ app/helpers/health_examination_results_helper.rb
@@ -1,6 +1,11 @@
 module HealthExaminationResultsHelper
   def data_check
-    if !!@health_examination_result.height && !!@health_examination_result.weight && !!@health_examination_result.ldl_cholesterol && !!@health_examination_result.fpg && !!@health_examination_result.gamma_gtp && !!@health_examination_result.uric_acid
+    
+    if !@health_examination_result.any?
+      return false
+    elsif !!@health_examination_result.height && !!@health_examination_result.weight &&
+      !!@health_examination_result.ldl_cholesterol && !!@health_examination_result.fpg &&
+      !!@health_examination_result.gamma_gtp && !!@health_examination_result.uric_acid
       return true
     end
   end
