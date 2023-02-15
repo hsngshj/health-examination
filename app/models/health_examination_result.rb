35bb5bdeec02e37f91d4798da849523b8b758ebb
Ubuntu <ubuntu@ip-172-31-12-101.ap-northeast-1.compute.internal>
2023-02-15 13:15:23 +0000

    add examination_file_path validation

--- app/models/health_examination_result.rb
+++ app/models/health_examination_result.rb
@@ -1,5 +1,6 @@
 class HealthExaminationResult < ApplicationRecord
   belongs_to :user
-  
+  validates :examination_file_path, presence: true
+
   mount_uploader :examination_file_path, ImageUploader
 end
