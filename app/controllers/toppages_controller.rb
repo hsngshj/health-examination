688faae7d6d4a69f6b240a10768944e8b28e8221
Ubuntu <ubuntu@ip-172-31-12-101.ap-northeast-1.compute.internal>
2023-02-16 07:08:46 +0000

    update radar_chart display logic

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -30,25 +30,27 @@ class ToppagesController < ApplicationController
     @health_examination_standard_result = HealthExaminationStandardResult.where(age: @age, gender: @user.gender).first
     
     @bmi = @health_examination_result.weight / (@health_examination_result.height / 100 ) ** 2
-    @bmi_standard = @health_examination_standard_result.bmi
-    @bmi_radar = @bmi / @bmi_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出
-    
-    
     @ldl_cholesterol = @health_examination_result.ldl_cholesterol
-    @ldl_cholesterol_standard = @health_examination_standard_result.ldl_cholesterol
-    @ldl_cholesterol_radar = @ldl_cholesterol / @ldl_cholesterol_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出    
-    
     @fpg = @health_examination_result.fpg
-    @fpg_standard = @health_examination_standard_result.fpg
-    @fpg_radar = @fpg / @fpg_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出    
-    
     @gamma_gtp = @health_examination_result.gamma_gtp
-    @gamma_gtp_standard = @health_examination_standard_result.gamma_gtp
-    @gamma_gtp_radar = @gamma_gtp / @gamma_gtp_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出    
-    
     @uric_acid = @health_examination_result.uric_acid
-    @uric_acid_standard = @health_examination_standard_result.uric_acid
-    @uric_acid_radar = @uric_acid / @uric_acid_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出
+    
+    if !!@health_examination_standard_result
+      @bmi_standard = @health_examination_standard_result.bmi
+      @bmi_radar = @bmi / @bmi_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出
+      
+      @ldl_cholesterol_standard = @health_examination_standard_result.ldl_cholesterol
+      @ldl_cholesterol_radar = @ldl_cholesterol / @ldl_cholesterol_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出    
+      
+      @fpg_standard = @health_examination_standard_result.fpg
+      @fpg_radar = @fpg / @fpg_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出    
+      
+      @gamma_gtp_standard = @health_examination_standard_result.gamma_gtp
+      @gamma_gtp_radar = @gamma_gtp / @gamma_gtp_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出    
+      
+      @uric_acid_standard = @health_examination_standard_result.uric_acid
+      @uric_acid_radar = @uric_acid / @uric_acid_standard * 80 # レーダーチャート用。標準値を80としたときの診断結果の値を算出
+    end
   end
   
   def set_linechart_data
